6abec2ec2ef93415c0645ff84ed124588c4703c1
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-01-25 01:12:49 +0900

    Add register page

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login),
+    path("register/", views.register),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.contrib import auth
 from django.shortcuts import render, redirect
 from .base import get_data
 
+UserModel = auth.get_user_model()
+
 
 def login(request):
     data = get_data(request)
@@ -18,3 +20,19 @@ def login(request):
         else:
             data['invalid'] = True
     return render(request, 'account/login.html', data)
+
+
+def register(request):
+    data = get_data(request)
+    if request.POST:
+        form = request.POST
+
+        _id = form.get('id')
+        _name = form.get('name')
+        _phone = form.get('phone')
+        _pw = form.get('pw')
+
+        user = UserModel.objects.create_user(_id, _name, _phone, _pw)
+        return redirect('/account/login')
+
+    return render(request, 'account/register.html', data)
