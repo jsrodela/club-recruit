943688ffe58fd6b1e69c18fc870416343115cf5c
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-01 14:28:04 +0900

    Add about and form to installed apps

--- jamsinclub/settings.py
+++ jamsinclub/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'about',
     'account',
     'index',
+    'form',
     'jamsinclub',
     "django.contrib.admin",
     "django.contrib.auth",
