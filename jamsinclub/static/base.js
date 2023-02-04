4435d75c7b600a8b1ba8eb6d4c939a08ef8c67a4
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-05 02:14:54 +0900

    Add fade transition effect

--- jamsinclub/static/base.js
+++ jamsinclub/static/base.js
@@ -3,8 +3,16 @@ function changeNav(element, link) {
     document.querySelector('nav > ul > li.selected').classList.remove('selected')
     element.classList.add('selected')
 
-    $('.wrapper').load(link + ' .wrapper2', function() {
+    wrapper = $('.wrapper')
+    wrapper.addClass('fade')
+    setTimeout(function() {
+        wrapper.load(link + ' .wrapper2', function() {
         history.pushState({page_id: 1}, '', link)
-    })
+        setTimeout(function() {
+            wrapper.removeClass('fade')
+        }, 100) // 0.1s to apply style
+        })
+    }, 300) // 0.3s to fadeout
+
 
 }
