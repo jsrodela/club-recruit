0c01893603a8f6a171afeba6afe14c1fff5878d4
9ueenrena <9ueenrena@gmail.com>
2023-02-10 10:54:47 +0900

    db 만듬

--- form/models.py
+++ form/models.py
@@ -1,3 +1,17 @@
+import json
+
 from django.db import models
 
 # Create your models here.
+
+class FormModel(models.Model):
+    number = models.IntegerField()
+
+    club = models.CharField(
+        max_length=10
+    )
+
+    section = models.JSONField(
+        encoder=json.JSONEncoder,
+        decoder=json.JSONDecoder
+    )
