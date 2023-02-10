099b607cfa476fe81172bf08da070743751338fc
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-10 15:04:04 +0900

    Add submit handling

--- form/admin.py
+++ form/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from form.models import FormModel
+
+
+class FormModelAdmin(admin.ModelAdmin):
+    fields = ['number', 'club', 'section']
+
+
+admin.site.register(FormModel)
