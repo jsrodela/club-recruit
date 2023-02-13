e8f64f37129546a978b68c53797d3bf2ec63780a
9ueenrena <9ueenrena@gmail.com>
2023-02-13 14:31:17 +0900

    asdf

--- index/views.py
+++ index/views.py
@@ -13,7 +13,7 @@ def index(request):
 
     for i in array:
         club_names.append(i.club)  # club_names라는 리스트에 amas.club rodela.club 처럼 신청한 동아리.club만 추가
-    print(club_names)
+
 
     data['club_names'] = club_names
 
