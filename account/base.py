e43887816f724f5c6390f4d0939c61a9aa160231
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-13 15:09:19 +0900

    Update base.html and base.py to match club model

--- account/base.py
+++ account/base.py
@@ -1,9 +1,26 @@
 from django.contrib import auth
 
+from about.models import ClubModel
+
 
 def get_data(request):
+    data = {}
+
     user = auth.get_user(request)
     if user.is_authenticated:
-        return {'user': user}
+        data['user'] = user
     else:
-        return {}
+        pass
+
+    clubs = []
+    for club in ClubModel.objects.all():
+        obj = {
+            'name': club.name,
+            'code': club.code,
+        }
+        if club.logo_image is not None:
+            obj['club_logo'] = club.logo_image.image.url
+        clubs.append(obj)
+    data['clubs'] = clubs
+
+    return data
