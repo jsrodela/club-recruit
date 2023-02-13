f5e5de959844f06f78891d0068a09c643ee03ae8
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-13 15:20:33 +0900

    Update ClubModel code as unique

--- about/models.py
+++ about/models.py
@@ -14,7 +14,7 @@ class ClubModel(models.Model):
     name = models.CharField(max_length=100, default='테스트_동아리')
 
     # 동아리 코드 (영문 소문자, 10자 이내)
-    code = models.CharField(max_length=10, default='test_club')  # always english lowercase
+    code = models.CharField(max_length=10, default='test_club', unique=True)  # always english lowercase
 
     # 지원 시작 ~ 종료 시간
     form_start = models.DateTimeField(auto_now=True)
