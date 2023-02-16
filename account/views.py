a4b5849eff80d159c7b10aa5d077c07caf750b2f
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-16 11:58:04 +0900

    Update register

--- account/views.py
+++ account/views.py
@@ -32,7 +32,16 @@ def register(request):
         _phone = form.get('phone')
         _pw = form.get('pw')
 
-        user = UserModel.objects.create_user(_id, _name, _phone, _pw)
-        return redirect('/account/login')
+        if UserModel.objects.filter(id=_id).exists():
+            data['invalid'] = f"학번이 {_id}인 계정이 이미 존재합니다."
+        else:
+            user = UserModel.objects.create_user(_id, _name, _phone, _pw)
+            auth.login(request, user)
+            return redirect('/')
 
     return render(request, 'account/register.html', data)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
