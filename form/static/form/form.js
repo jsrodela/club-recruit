823b7f4bf6f47ee61df150a0123c62c9efe26bd7
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-02-20 11:21:11 +0900

    Add form textarea auto height

--- form/static/form/form.js
+++ form/static/form/form.js
@@ -82,3 +82,13 @@ else {
         }
     }
 }
+
+/* textarea height */
+function textarea_resize(element) {
+    element.style.height = "";
+    element.style.height = element.scrollHeight + "px"
+}
+
+for (let element of document.getElementsByTagName('textarea')) {
+    textarea_resize(element);
+}
