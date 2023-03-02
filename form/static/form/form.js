b1abe4d4508bf4b73da2f1eee31438a22edd4cf8
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-03-02 16:04:59 +0900

    Disable require temporary

--- form/static/form/form.js
+++ form/static/form/form.js
@@ -81,12 +81,12 @@ else {
         }
     }
 
-    for (let require of required) {
-        let elements = document.querySelectorAll(`input[name="${require}"`)
-        for (let element of elements) {
-            element.required = true;
-        }
-    }
+//    for (let require of required) {
+//        let elements = document.querySelectorAll(`input[name="${require}"]`)
+//        for (let element of elements) {
+//            element.required = true;
+//        }
+//    }
 }
 
 /* textarea height */
