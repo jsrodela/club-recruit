65bb558a595bd3a07b135625f643777715c4a1e7
hurdoo <64835865+HURDOO@users.noreply.github.com>
2023-03-04 11:27:24 +0900

    Add banner transition

--- index/views.py
+++ index/views.py
@@ -13,9 +13,10 @@ def index(request):
     if 'user' in data:
         data['forms'] = FormModel.objects.filter(number=data['user'].id, archive=False)
 
-    if 'banner_club' in request.GET:  # /?banner_club=CODE
-        data['banner_club'] = ClubModel.objects.get(code=request.GET.get('banner_club'))
-    elif ClubModel.objects.all().exists():  # 맨 처음에 동아리 없을 때 제외
-        data['banner_club'] = random.choice(list(ClubModel.objects.all()))
+    # if 'banner_club' in request.GET:  # /?banner_club=CODE
+    #     data['banner_club'] = ClubModel.objects.get(code=request.GET.get('banner_club'))
+    # elif ClubModel.objects.all().exists():  # 맨 처음에 동아리 없을 때 제외
+    #     data['banner_club'] = random.choice(list(ClubModel.objects.all()))
+    data['banner_clubs'] = ClubModel.objects.all()
 
     return render(request, 'index/index.html', data)
