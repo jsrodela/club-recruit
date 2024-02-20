1851ad87ed2aa7d4cb2c2aa8159675fd9d2b282a
HURDOO <64835865+HURDOO@users.noreply.github.com>
2024-02-21 00:42:57 +0900

    Add status

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from index.models import StatusModel
+
+
 # Register your models here.
+
+
+class StatusModelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(StatusModel, StatusModelAdmin)
+
--- index/models.py
+++ index/models.py
@@ -2,15 +2,18 @@ import json
 
 from django.db import models
 
+
 # Create your models here.
-class MyPageModel(models.Model):
-
-    circle = models.JSONField(
-        encoder=json.JSONEncoder,
-        decoder=json.JSONDecoder
-    )
-
-    interview_time = models.JSONField(
-        encoder=json.JSONEncoder,
-        decoder=json.JSONDecoder
-    )
+class StatusModel(models.Model):
+    STATUS = [('PREPARE', '동아리 홍보 기간'),
+              ('FIRST_FORM', '1차 서류 지원'),
+              ('FIRST_RESULT', '1차 서류합격 발표'),
+              ('SECOND_INTERVIEW', '2차 면접 기간'),
+              ('SECOND_RESULT', '최초합 발표'),
+              ('ADD_FIRST_RESULT', '1차 추가합격 발표'),
+              ('ADD_SECOND_RESULT', '2차 추가합격 발표'),
+              ('ADD_THIRD_RESULT', '3차 추가합격 발표'),
+              ('ADDITIONAL', '추가모집 기간'),
+              ('FINISH', '상설동아리 모집 종료')
+              ]
+    current = models.CharField(max_length=20, choices=STATUS, null=False, default='PREPARE')
