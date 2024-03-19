c4f3a0f29c77ab239c8f1b4874b402ea6298d011
HURDOO <64835865+HURDOO@users.noreply.github.com>
2024-03-20 01:55:34 +0900

    Add notice

--- index/models.py
+++ index/models.py
@@ -13,7 +13,10 @@ class StatusModel(models.Model):
               ('ADD_FIRST_RESULT', '1차 추가합격 발표'),
               ('ADD_SECOND_RESULT', '2차 추가합격 발표'),
               ('ADD_THIRD_RESULT', '3차 추가합격 발표'),
+              ('ADD_FOURTH_RESULT', '4차 추가합격 발표'),
               ('ADDITIONAL', '추가모집 기간'),
               ('FINISH', '상설동아리 모집 종료')
               ]
     current = models.CharField(max_length=20, choices=STATUS, null=False, default='PREPARE')
+
+    notice = models.TextField(default="", blank=True)
